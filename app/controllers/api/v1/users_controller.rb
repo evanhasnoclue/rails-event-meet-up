03c1a92ae7e9992f2e95794ecdd244e2490e3fe1
BorisFeng <boris.feng@decathlon.com>
2018-11-19 02:16:24 -0800

    updated user controller and view

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to user_path(@user), status: :created
     else
       render :new
     end
@@ -26,7 +26,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       # redirect_to restaurant_path(@restaurant)
       render :show
     else
-      rende_error
+      render_error
     end
   end
 
@@ -41,4 +41,8 @@ class Api::V1::UsersController < Api::V1::BaseController
     @user = User.find(params[:id])
   end
 
+  def render_error
+    render json: { errors: @user.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
