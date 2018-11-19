03d4b708ffedafcb9ca816d9317d5264596a299f
kai wang <wangkai199385@126.com>
2018-11-19 17:49:17 +0800

    added update and destroy actions to event controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def create
     @user = User.find(params[:user_id])
-    @event = Event.new
+    @event = Event.new(event_params)
     @event.user = @user
     if @event.save
       render :show, status: :created
@@ -18,7 +18,26 @@ class Api::V1::UsersController < Api::V1::BaseController
     end
   end
 
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    head :no_content
+  end
+
   private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :time, :place, :deadline, :capacity, :photo)
+  end
 
   def render_error
     render json: { errors: @event.errors.full_messages },
