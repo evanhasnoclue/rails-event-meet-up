ad8e187b8b504650147e5d8d06ef0c85b6d19b0b
kai wang <wangkai199385@126.com>
2018-11-19 17:44:20 +0800

    added create action to event controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,4 +6,23 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
     @event = Event.find(params[:id])
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @event = Event.new
+    @event.user = @user
+    if @event.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def render_error
+    render json: { errors: @event.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
 end
