e7038acc4e1affe1057e1c166f0b019d9342c612
kai wang <wangkai199385@126.com>
2018-11-20 17:59:58 +0800

    impliment login and create user, added uniqueness on wechat_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
   validates :name, presence: true
+  validates :wechat_id, uniqueness: true
   # validates :phone, uniqueness: true
 end
