d25086f15c2615aeb72561533b01661bff22e217
BorisFeng <boris.feng@decathlon.com>
2018-11-19 19:36:57 -0800

    get api for user and event worked

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews, through: :bookings
   validates :title, presence: true
   # validates :description, presence: true
   # validates :time, presence: true
