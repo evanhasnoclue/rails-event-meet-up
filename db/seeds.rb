78586815398baaddcddfceaefbfbad103145c763
BorisFeng <boris.feng@decathlon.com>
2018-11-20 23:15:44 -0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,23 +16,23 @@ User.create(name: "Boris", wechat_id: "2")
 
 Event.destroy_all
 Event.create(user_id: 1,  title: "千山暮雪",
-        address: "Mountain in the sky",
-        description: "10 people",
+        address: "jingan temple",
+        description: "MOUNTAIN IN THE SKY",
         capacity: 10,
-        photo: "http://2.bp.blogspot.com/-x1kTluVwm7w/UUMn89GYdMI/AAAAAAAAUjI/rYf1pdikUws/s1600/Nature+Scenery+Wallpapers+1.jpg")
+        photo: "http://wallpapercave.com/wp/PzaH3FR.jpg")
 Event.create(user_id: 1, title: "田园新村",
-        address: "90 TIANYUAN ZHENG",
-        description: "7 people",
+        address: "beijing west road",
+        description: "90 TIANYUAN ZHENG",
         capacity: 10,
         photo: "http://russiatrek.org/blog/wp-content/uploads/2013/05/beautiful-scenery-of-the-mountain-ingushetia-russia-11.jpg")
 Event.create(user_id: 2, title: "极地风光",
-        address: "EXTREME EAST",
-        description: "30 people",
+        address: "nanjing west road",
+        description: "EXTREME EAST",
         capacity: 30,
-        photo: "http://4.bp.blogspot.com/-tcIOYxa5akg/T-spBfyFBZI/AAAAAAAAAyQ/Q6zLAg_mFoQ/s1600/natural-scenery-7.jpg")
+        photo: "https://www4.bing.com/th?id=OIP.rlD48_vU1S_e3LrzA4i5qAHaEo&pid=Api&w=1600&h=1000&rs=1&p=0")
 Event.create(user_id: 2, title: "樱花烂漫时",
-        address: "FlOWER ZONE",
-        description: "20 people",
+        address: "changning road",
+        description: "FlOWER ZONE",
         capacity: 20,
         photo: "http://www.dvd-ppt-slideshow.com/blog/wp-content/uploads/2010/07/natural-scenery-13.jpg")
 # Event.create(user_id: 3, title: "test3", description: "cc", time: "", place: "", capacity: 10, deadline: "")
